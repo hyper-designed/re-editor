20fbc032ffdab2a66e72f298d6cf31eefb629e67
BirjuVachhani <brvachhani@gmail.com>
2026-07-04 22:07:55 +0530

    :art: code format and lint fixes

--- test/code_line_extension_test.dart
+++ test/code_line_extension_test.dart
@@ -4,37 +4,23 @@ import 'package:re_editor/re_editor.dart';
 void main() {
   group('CodeLineExtension', () {
     test('get textLines', () {
-      expect(''.textLines, const [
-        ''
-      ]);
-      expect('abc'.textLines, const [
-        'abc'
-      ]);
-      expect('abc\nfoo\nbar'.textLines, const [
-        'abc',
-        'foo',
-        'bar'
-      ]);
-      expect('abc\r\nfoo\rbar'.textLines, const [
-        'abc',
-        'foo',
-        'bar'
-      ]);
+      expect(''.textLines, const ['']);
+      expect('abc'.textLines, const ['abc']);
+      expect('abc\nfoo\nbar'.textLines, const ['abc', 'foo', 'bar']);
+      expect('abc\r\nfoo\rbar'.textLines, const ['abc', 'foo', 'bar']);
     });
     test('get codeLines', () {
-      expect('abc\nfoo\nbar'.codeLines, CodeLines.of(const [
-        CodeLine('abc'),
-        CodeLine('foo'),
-        CodeLine('bar')
-      ]));
+      expect(
+          'abc\nfoo\nbar'.codeLines,
+          CodeLines.of(
+              const [CodeLine('abc'), CodeLine('foo'), CodeLine('bar')]));
     });
 
     test('get codeLinesAsync', () async {
-      expect(await 'abc\nfoo\nbar'.codeLinesAsync, CodeLines.of(const [
-        CodeLine('abc'),
-        CodeLine('foo'),
-        CodeLine('bar')
-      ]));
+      expect(
+          await 'abc\nfoo\nbar'.codeLinesAsync,
+          CodeLines.of(
+              const [CodeLine('abc'), CodeLine('foo'), CodeLine('bar')]));
     });
   });
 }
